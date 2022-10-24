fd79c9cf3a4dbfca00bb3c75526efab5b23a6acd
Pasha <askerovpasha21@gmail.com>
2022-10-24 23:58:40 +0400

    main-4

--- swiper.js
+++ swiper.js
@@ -38,3 +38,50 @@ const swiper3 = new Swiper(".main-swiper-3", {
       1140: {slidesPerView: 4,spaceBetween: 20},
       },
   });
+  const swiper4 = new Swiper(".main-swiper-4", {
+    autoplay: {
+    delay: 5000,
+    },
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {slidesPerView: 2,spaceBetween: 20},
+      992: {slidesPerView: 3,spaceBetween: 20},
+      },
+  });
+  const swiper5 = new Swiper(".main-swiper-5", {
+    autoplay: {
+    delay: 5000,
+    },
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {slidesPerView: 2,spaceBetween: 20},
+      992: {slidesPerView: 3,spaceBetween: 20},
+      },
+  });
+  const swiper6 = new Swiper(".main-swiper-6", {
+    autoplay: {
+    delay: 5000,
+    },
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {slidesPerView: 2,spaceBetween: 20},
+      992: {slidesPerView: 3,spaceBetween: 20},
+      },
+  });
